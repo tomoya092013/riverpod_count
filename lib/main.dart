ea37c278adf712819f938a8eeea0d7582b60bd0a
根岸智也 <negishitomoya@negishitomoyanoMacBook-Pro.local>
2025-12-19 06:44:14 +0900

    ConsumerStatefulWidgetの使い方

--- lib/main.dart
+++ lib/main.dart
@@ -19,48 +19,39 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends ConsumerStatefulWidget {
   MyHomePage({super.key});
 
+  @override
+  ConsumerState<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends ConsumerState<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Consumer(
-          builder: (BuildContext context, WidgetRef ref, Widget? child) =>
-              Text(ref.watch(titleProvider)),
-        ),
+        title: Text(ref.watch(titleProvider)),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: .center,
           children: [
-            Consumer(
-              builder: (context, ref, child) =>
-                  Text(ref.watch(messageProvider)),
-            ),
+            Text(ref.watch(messageProvider)),
             const SizedBox(height: 16),
             const Text('You have pushed the button this many times:'),
-            Consumer(
-              builder: (context, ref, child) {
-                return Text(
-                  ref.watch(countProvider).toString(),
-                  style: Theme.of(context).textTheme.headlineMedium,
-                );
-              },
+            Text(
+              ref.watch(countProvider).toString(),
+              style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
         ),
       ),
-      floatingActionButton: Consumer(
-        builder: (context, ref, child) {
-          return FloatingActionButton(
-            onPressed: () => ref.read(countProvider.notifier).state++,
-            tooltip: 'Increment',
-            child: const Icon(Icons.add),
-          );
-        },
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => ref.read(countProvider.notifier).state++,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
       ),
     );
   }
